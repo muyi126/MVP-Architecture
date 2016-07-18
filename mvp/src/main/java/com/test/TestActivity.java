744f57f9e0289dd9aa84986fdd46efc7a3cabe36
givon <muyi126@163.com>
2016-07-18 09:41:37 +0800

    测试Activity

---
+++ mvp/src/main/java/com/test/TestActivity.java
@@ -0,0 +1,15 @@
+package com.test;
+
+/**
+ *
+ *
+ * Copyright 2016 Givon All rights reserved.
+ * Givon PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ * @author givon
+ * @version 1.0
+ * @六月 16/6/29 下午10:37 - Guzhu
+ * @email:muyi126@163.com
+ */
+public class TestActivity {
+}
